0b293e4e5f095198469cf78ad759617c2b45f34e
gyeongho <gyeongho@localhost>
2022-05-12 15:28:39 +0900

    test1

---
+++ exam/src/exam/IngeritanceApp.java
@@ -0,0 +1,15 @@
+package exam;
+
+class Cal{
+	public int sum(int v1,int v2) {
+		return v1+v2;
+	}
+}
+public class IngeritanceApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
